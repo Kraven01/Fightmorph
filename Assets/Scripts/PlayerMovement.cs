4884db80ed0a77a62d8cfe40a2341b0847de6e87
Kraven01 <73736702+Kraven01@users.noreply.github.com>
2023-05-20 10:37:03 +0200

    Enemies now follow the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,34 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : Movement
 {
-
-    public float moveSpeed = 5f;
-
-    public Rigidbody2D rb;
-    public Animator animator;
-
-    Vector2 movement;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        if (!dead)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("movement_X", movement.x);
-        animator.SetFloat("movement_Y", movement.y);
-        animator.SetFloat("speed", movement.sqrMagnitude);
-
+            base.Update();
+        }
     }
 
-    void FixedUpdate()
+    public override void FixedUpdate()
     {
         if (movement.x == 1)
         {
@@ -39,6 +29,6 @@ public class PlayerMovement : MonoBehaviour
         {
             GetComponent<PlayerCombat>().right = false;
         }
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        base.FixedUpdate();
     }
 }
